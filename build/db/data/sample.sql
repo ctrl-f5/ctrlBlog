be66243065af5f55f8a865b36bb033c86a43113f
ctrl-f5 <mythix.dev@gmail.com>
2012-11-18 01:36:08 +0100

    added guest role and sample privileges for superuser

--- build/db/data/sample.sql
+++ build/db/data/sample.sql
@@ -11,15 +11,21 @@ INSERT INTO `ctrl_auth_user` (`id`, `username`, `password`, `email`) VALUES
 
 INSERT INTO `ctrl_auth_role` (`id`, `name`) VALUES
 (1, 'superuser'),
-(1, 'guest');
+(2, 'guest');
 
 INSERT INTO `ctrl_auth_user_role` (`role_id`, `user_id`) VALUES
 ('1', '1');
 
 INSERT INTO  `ctrl_auth_resource` (`id`, `resource`)VALUES
-(1 , 'global');
+(1 , 'global'),
+(2 , 'routes.Ctrl\\Module\\Auth\\Controller\\Permission.index'),
+(3 , 'routes.Ctrl\\Module\\Auth\\Controller\\User.index'),
+(4 , 'routes.Ctrl\\Module\\Auth\\Controller\\Index.index');
 
 INSERT INTO  `ctrl_blog`.`ctrl_auth_permission` (`id` , `role_id` , `resource_id` , `isAllowed`) VALUES
-(1 , '1',  '1',  '1');
+(1 , '1',  '1',  '1'),
+(2 , '1',  '2',  '1'),
+(3 , '1',  '3',  '1'),
+(4 , '1',  '4',  '1');
 
 SET foreign_key_checks = 1;
