ee1ba7c1b79f45ac5c953b1e2fe9e0aea5cf07c0
ctrl-f5 <mythix.dev@gmail.com>
2012-11-19 22:56:16 +0100

    updated dataset to adapt ctrlAuth module changes

--- build/db/data/sample.sql
+++ build/db/data/sample.sql
@@ -1,31 +1,4 @@
-SET foreign_key_checks = 0;
 
-INSERT INTO `article` (`id`, `title`, `content`, `dateCreated`) VALUES
-(1, 'dsafas', 'dsadsadsadsa\r\ndsa\r\nfd\r\ngsfdg\r\nfh\r\nghdyty\r\nbg\r\nfgfdsgfdgsdf', '2012-11-15 13:00:00');
-
-INSERT INTO `blog_user` (`id`, `ctrlAuthUser_id`, `displayName`) VALUES
-(1, 1, 'John Doe');
-
-INSERT INTO `ctrl_auth_user` (`id`, `username`, `password`, `email`) VALUES
-(1, 'admin', 'd033e22ae348aeb5660fc2140aec35850c4da997', 'example@example.com');
-
-INSERT INTO `ctrl_auth_role` (`id`, `name`) VALUES
-(1, 'superuser'),
-(2, 'guest');
-
-INSERT INTO `ctrl_auth_user_role` (`role_id`, `user_id`) VALUES
-('1', '1');
-
-INSERT INTO  `ctrl_auth_resource` (`id`, `resource`)VALUES
-(1 , 'global'),
-(2 , 'routes.Ctrl\\Module\\Auth\\Controller\\Permission.index'),
-(3 , 'routes.Ctrl\\Module\\Auth\\Controller\\User.index'),
-(4 , 'routes.Ctrl\\Module\\Auth\\Controller\\Index.index');
-
-INSERT INTO  `ctrl_blog`.`ctrl_auth_permission` (`id` , `role_id` , `resource_id` , `isAllowed`) VALUES
-(1 , '1',  '1',  '1'),
-(2 , '1',  '2',  '1'),
-(3 , '1',  '3',  '1'),
-(4 , '1',  '4',  '1');
-
-SET foreign_key_checks = 1;
+source ./../vendor/ctrl-f5/ctrlAuth/db/data/default-data.sql
+source ./db/data/auth.sql
+source ./db/data/blog.sql
